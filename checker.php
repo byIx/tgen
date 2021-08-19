441b0e29c6588eaea4f1a942604db7ba04932e22
byIx <40878600+byIx@users.noreply.github.com>
2021-08-20 02:52:29 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"2.0.0", "longversion":20000, "released":1629219600, "updateurl":"https:\/\/ipbmafia.ru\/topic\/13427-traffic\/" }
+{ "version":"2.0.0", "longversion":20000, "released":1626361200, "updateurl":"https:\/\/ipbmafia.ru\/topic\/13427-traffic\/" }
