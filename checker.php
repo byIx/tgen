932d703c670ef7952844aea78fe254246ae24f50
byIx <40878600+byIx@users.noreply.github.com>
2022-03-20 03:27:27 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"2.0.2", "longversion":20002, "released":1647720000, "updateurl":"https:\/\/github.com\/byIx\/oneip/\releases\/" }
+{ "version":"2.0.2", "longversion":20002, "released":1647720000, "updateurl":"https:\/\/github.com\/byIx\/tgen\/releases\/" }
