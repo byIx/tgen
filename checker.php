00374dda512c25d7f84361139e5fb3fb35ec228a
byIx <40878600+byIx@users.noreply.github.com>
2022-06-22 02:30:21 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"2.1.0", "longversion":21000, "released":1655834400, "updateurl":"https:\/\/github.com\/byIx\/tgen\/releases\/" }
+{ "version":"2.1.0", "longversion":21000, "released":1655834400, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/111\/" }
