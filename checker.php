bd24f9ff7fe3d425abf96e7cf407ae824c56155b
byIx <40878600+byIx@users.noreply.github.com>
2022-06-22 04:13:00 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"2.1.0", "longversion":21000, "released":1655834400, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/111\/" }
+{ "version":"2.1.0", "longversion":21000, "released":1655834400, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2681-invk4-traffic-generator-re\/" }
